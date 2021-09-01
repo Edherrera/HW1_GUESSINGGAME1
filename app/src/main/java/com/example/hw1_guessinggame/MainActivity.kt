9ccfc96d949c871461e536b4b5ff8b92a4aff4a3
Edherrera <edherrera96@uan.edu.co>
2021-08-31 22:51:38 -0500

    primer commit

---
+++ app/src/main/java/com/example/hw1_guessinggame/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.hw1_guessinggame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import kotlin.random.Random
+
+
+class MainActivity : AppCompatActivity() {
+
+    var randomNumber : Int = Random.nextInt(1,1000)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun myFunction(p: View){
+        val editNumber : EditText = findViewById(R.id.editNumber)
+        val myTextView : TextView = findViewById(R.id.myText)
+        val num = editNumber.text.toString()
+
+
+        //Toast.makeText(this, "Hello ${num}!!", Toast.LENGTH_LONG).show()
+        myTextView.text = "Ingresaste el: ${num}"
+        /*
+        if (num == randomNumber){
+            myTextView.text = "ganaste: ${num}"
+        }else{
+            myTextView.text = "perdiste: ${num}"
+        }
+        */
+
+    }
+
+
+}
